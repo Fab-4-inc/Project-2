bfbe2d0ac0396614ee77c795f20a31fd0effb25f
Pamela Saborio <pame1481@gmail.com>
2019-12-19 21:10:44 -0600

    --all

--- app.py
+++ app.py
@@ -17,9 +17,10 @@ db = SQLAlchemy(app)
 Base = automap_base()
 Base.prepare(db.engine, reflect=True)
 
-Redfin = Base.classes.redfin_aug2019
-Divvy_stationinfo = Base.classes.divvy_stationinfo
-Divvy_stationstatus = Base.classes.divvy_stationstatus
+# Redfin = Base.classes.redfin_aug2019
+# Divvy_stationinfo = Base.classes.divvy_stationinfo
+# Divvy_stationstatus = Base.classes.divvy_stationstatus
+Redfin_Divvy = Base.classes.merged_redfin_divvy
 
 @app.route("/")
 def home_page():
@@ -30,45 +31,75 @@ def home_page():
 # # Steve
 
 
-@app.route("/Redfin")
-def Redfin():
-    select = [Redfin.Neighborhood, 
-    Redfin.Lat, 
-    Redfin.Lng, 
-    Redfin.Sale_Price, 
-    Redfin.Homes_Sold,
-    Redfin.New_Listings,
-    Redfin.Days_on_Market]
+# @app.route("/Redfin")
+# def Redfin():
+#     select = [Redfin.Neighborhood, 
+#     Redfin.Lat, 
+#     Redfin.Lng, 
+#     Redfin.Sale_Price, 
+#     Redfin.Homes_Sold,
+#     Redfin.New_Listings,
+#     Redfin.Days_on_Market]
 
-    results = db.session.query(*select).all()
-    print(results)
+#     results = db.session.query(*select).all()
+#     print(results)
 
-    return "Success"
+#     return "Success"
 
 
-@app.route("/DivvyStationInfo")
+# @app.route("/DivvyStationInfo")
+# def DivvyInfo():
+#     select = [Divvy_stationinfo.lat, 
+#     Divvy_stationinfo.lon, 
+#     Divvy_stationinfo.name, 
+#     Divvy_stationinfo.station_id]
+
+#     results = db.session.query(*select).all()
+#     print(results)
+
+#     return "Success"
+
+@app.route("/api/redfin_divvy")
 def DivvyInfo():
-    select = [Divvy_stationinfo.lat, 
-    Divvy_stationinfo.lon, 
-    Divvy_stationinfo.name, 
-    Divvy_stationinfo.station_id]
+    select = [Redfin_Divvy.city, 
+    Redfin_Divvy.neighborhood, 
+    Redfin_Divvy.redfin_lat, 
+    Redfin_Divvy.redfin_lng,
+    Redfin_Divvy.month,
+    Redfin_Divvy.sale_price,
+    Redfin_Divvy.homes_sold,
+    Redfin_Divvy.new_listings,
+    Redfin_Divvy.days_on_market,
+    Redfin_Divvy.last_reported,
+    Redfin_Divvy.num_bikes_available,
+    Redfin_Divvy.num_bikes_disabled,
+    Redfin_Divvy.num_docks_available,
+    Redfin_Divvy.num_docks_disabled,
+    Redfin_Divvy.num_ebikes_available,
+    Redfin_Divvy.station_id,
+    Redfin_Divvy.capacity,
+    Redfin_Divvy.divvy_lat,
+    Redfin_Divvy.divvy_lng,
+    Redfin_Divvy.name,
+    Redfin_Divvy.count]
 
     results = db.session.query(*select).all()
     print(results)
 
-    return "Success"
+    return csv_results
+    # return "Success"
 
-@app.route("/DivvyStationStatus")
-def DivvyInfo():
-    select = [Divvy_stationstatus.is_installed, 
-    Divvy_stationstatus.num_docks_available, 
-    Divvy_stationstatus.num_ebikes_available, 
-    Divvy_stationstatus.station_id]
+# @app.route("/DivvyStationStatus")
+# def DivvyInfo():
+#     select = [Divvy_stationstatus.is_installed, 
+#     Divvy_stationstatus.num_docks_available, 
+#     Divvy_stationstatus.num_ebikes_available, 
+#     Divvy_stationstatus.station_id]
 
-    results = db.session.query(*select).all()
-    print(results)
+#     results = db.session.query(*select).all()
+#     print(results)
 
-    return "Success"
+#     return "Success"
 
 # @app.route("/")
 # # Rachel
